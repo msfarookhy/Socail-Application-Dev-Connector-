5a723ba69455c5a7b4b32d63a8bde4138f1eba93
msfarookhy <msfarookhy@gmail.com>
2019-05-08 23:33:09 +0530

    MongoDb Connectivity With Mongoose

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://saif:76446272Saif@ds013192.mlab.com:13192/devconnector"
+};
--- server.js
+++ server.js
@@ -1,7 +1,16 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
 
+// DB Config
+const db = require("./config/keys").mongoURI;
+
+// connnect to MongoDB
+mongoose
+  .connect(db)
+  .then(() => console.log("Mongoose Connected"))
+  .catch(err => console.log(err));
 app.get("/", (req, res) => res.send("helo!"));
 
 const port = process.env.PORT || 7000;
