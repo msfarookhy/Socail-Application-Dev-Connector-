393ae3c1956b49a9c621fa7c5125c83f4b7edc4e
msfarookhy <msfarookhy@gmail.com>
2019-05-23 02:27:12 +0530

    Validation

--- Routes/api/users.js
+++ Routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load input validation
+const validateRegisterInput = require("../../validation/register");
+
 //Load User Model
 const User = require("../../models/User");
 
@@ -19,9 +22,17 @@ router.get("/test", (req, res) => res.json({ msg: "Hello Misbah" }));
 // @acess  Public
 
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  // Cheking validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
+
   User.findOne({ email: req.body.email }).then(user => {
     if (user) {
-      return res.status(400).json({ email: "Email is alredy Exists" });
+      errors.email = "Email is alredy Exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         s: "200", //Size
--- validation/register.js
+++ validation/register.js
@@ -1,13 +1,15 @@
 const validator = require("validator");
+const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
   let errors = {};
 
-  if (!validator.isLength(data.name, { min: 2, mas: 32 })) {
+  if (!validator.isLength(data.name, { min: 2, max: 32 })) {
     errors.name = "Name Must Be  Between 2 and 30 Charachtera ";
   }
+
   return {
     errors,
-    isvalid: errors
+    isvalid: isEmpty(errors)
   };
 };
