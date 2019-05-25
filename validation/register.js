73dff2d3ccdd9c145c38e0a122c7336db29751b8
msfarookhy <msfarookhy@gmail.com>
2019-05-25 22:48:40 +0530

    register validation complete

--- validation/register.js
+++ validation/register.js
@@ -1,4 +1,4 @@
-const validator = require("validator");
+const Validator = require("validator");
 const isEmpty = require("./is-empty");
 
 module.exports = function validateRegisterInput(data) {
@@ -9,35 +9,35 @@ module.exports = function validateRegisterInput(data) {
   data.password = !isEmpty(data.password) ? data.password : "";
   data.password2 = !isEmpty(data.password2) ? data.password2 : "";
 
-  if (!validator.isLength(data.name, { min: 2, max: 32 })) {
+  if (!Validator.isLength(data.name, { min: 2, max: 32 })) {
     errors.name = "Name Must Be  Between 2 and 32 Characters";
   }
 
-  if (validator.isEmpty(data.name)) {
+  if (Validator.isEmpty(data.name)) {
     errors.name = "Name Field Is Required";
   }
 
-  if (validator.isEmpty(data.email)) {
+  if (Validator.isEmpty(data.email)) {
     errors.email = "Email Field Is Required";
   }
 
-  if (!validator.isEmail(data.email)) {
+  if (!Validator.isEmail(data.email)) {
     errors.email = "Email Is Invalid";
   }
 
-  if (validator.isEmpty(data.password)) {
+  if (Validator.isEmpty(data.password)) {
     errors.password = "Password Field Is Required";
   }
 
-  if (!validator.isLength(data.password, { min: 6, max: 8 })) {
+  if (!Validator.isLength(data.password, { min: 6, max: 8 })) {
     errors.password = "Password Must Be At Least In 6 Character";
   }
 
-  if (validator.isEmpty(data.password2)) {
+  if (Validator.isEmpty(data.password2)) {
     errors.password2 = " Confirm Password Field Is Required";
   }
 
-  if (!validator.equals(data.password, data.password2)) {
+  if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords Shuld Be Match";
   }
 
