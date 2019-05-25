2e01a8e50170850771e58f719558b158e79a825f
msfarookhy <msfarookhy@gmail.com>
2019-05-25 23:12:11 +0530

    calidation completed login rout and Regestration validation complete

--- validation/login.js
+++ validation/login.js
@@ -7,12 +7,12 @@ module.exports = function validateLoginInput(data) {
   data.email = !isEmpty(data.email) ? data.email : "";
   data.password = !isEmpty(data.password) ? data.password : "";
 
-  if (Validator.isEmpty(data.email)) {
-    errors.email = "Email Field Is Required";
-  }
-
   if (!Validator.isEmail(data.email)) {
     errors.email = "Email Is Invalid";
+  }
+
+  if (Validator.isEmpty(data.email)) {
+    errors.email = "Email Field Is Required";
   }
 
   if (Validator.isEmpty(data.password)) {
