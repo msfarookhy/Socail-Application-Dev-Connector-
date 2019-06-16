6cfa1950e36b1422fd2c0751a0c6928beffed8f5
msfarookhy <msfarookhy@gmail.com>
2019-06-17 04:59:45 +0530

    added Delete route

--- Routes/api/profile.js
+++ Routes/api/profile.js
@@ -237,4 +237,28 @@ router.post(
   }
 );
 
+// @route  DELETE api/profile/experience:exp_id
+// @desc   Delete Experience profile
+// @acess  Private
+router.delete(
+  "/experience/:exp_id",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    Profile.findOne({ user: req.user.id })
+      .then(profile => {
+        // Get Remove index From experience array
+
+        const removeIndex = profile.experience
+          .map(item => item.id)
+          .indexOf(req.params.user_id);
+
+        //splice out of array from experience
+        profile.experience.splice(removeIndex, 1);
+        //Save
+        profile.save().then(profile => res.json(profile));
+      })
+      .catch(err => res.status(404).json(err));
+  }
+);
+
 module.exports = router;
