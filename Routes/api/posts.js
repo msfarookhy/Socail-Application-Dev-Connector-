ec554e5441403aba76a5b5d7b03a406079d74510
msfarookhy <msfarookhy@gmail.com>
2019-06-19 21:46:20 +0530

    created posts

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -3,7 +3,11 @@ const router = express.Router();
 const mongoose = require("mongoose");
 const passport = require("passport");
 
+// post model
 const Post = require("../../models/Post");
+
+//post validation
+const validatePostInput = require("../../validation/post");
 // @route  GET api/posts/test
 // @desc   Test posts Route
 // @acess  Public
@@ -17,10 +21,21 @@ router.post(
   "/",
   passport.authenticate("jwt", { session: false }),
   (req, res) => {
+    //destructring input post
+
+    const { errors, isValid } = validatePostInput(req.body);
+
+    //check validation
+
+    if (!isValid) {
+      //send errors in 400 in object
+      res.status(400).json(errors);
+    }
+
     const newPost = new Post({
       text: req.body.text,
       name: req.body.name,
-      avatar: req.body.name,
+      avatar: req.body.avatar,
       user: req.user.id
     });
 
