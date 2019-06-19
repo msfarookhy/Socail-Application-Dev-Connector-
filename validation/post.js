14b261725394c2e522bb7cdebe1ccb96e87cfe0b
msfarookhy <msfarookhy@gmail.com>
2019-06-19 20:37:29 +0530

    posts validation added

---
+++ validation/post.js
@@ -0,0 +1,20 @@
+const Validator = require("validator");
+const isEmpty = require("./is-empty");
+
+module.exports = function validatePostInput(data) {
+  let errors = {};
+
+  data.text = !isEmpty(data.text) ? data.text : "";
+
+  if (!validator.isLength(data.text, { min: 10, max: 100 })) {
+    errors.text = "Post Must Be In 10 To 300 Characters";
+  }
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text Field Is Required";
+  }
+
+  return {
+    errors,
+    isValid: isEmpty(errors)
+  };
+};
