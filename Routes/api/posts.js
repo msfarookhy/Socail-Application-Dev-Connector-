5d2aabdc2e1f56d7d8e9bd81d0fa6caf55d8e448
msfarookhy <msfarookhy@gmail.com>
2019-06-20 22:00:58 +0530

    Remove Comments logic Adeed

--- Routes/api/posts.js
+++ Routes/api/posts.js
@@ -159,4 +159,69 @@ router.post(
   }
 );
 
+// @route  Post api/posts/comment/:id
+// @desc   Comment On post
+// @acess  Private
+
+router.post(
+  "/comment/:id",
+  passport.authenticate("jwt", { seesion: false }),
+  (req, res) => {
+    //destructring
+    const { errors, isValid } = validatePostInput(req.body);
+    //check validation
+    if (!isValid) {
+      //send errors in 400 in object
+      res.status(400).json(errors);
+    }
+    Post.findById(req.params.id)
+      .then(post => {
+        const newComment = {
+          text: req.body.text,
+          name: req.body.name,
+          avatar: req.body.avatar,
+          user: req.user.id
+        };
+        // add comment to post
+        post.comments.unshift(newComment);
+
+        // save comment
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No Post Found" }));
+  }
+);
+
+// @route  DELETE api/posts/comment/:id/:comment_id
+// @desc   DELETE Comment From post
+// @acess  Private
+
+router.delete(
+  "/comment/:id/:comment_id",
+  passport.authenticate("jwt", { seesion: false }),
+  (req, res) => {
+    Post.findById(req.params.id)
+      .then(post => {
+        if (
+          post.comments.filter(
+            comment => comment.id.toString() === req.params.comment_id
+          ).length === 0
+        ) {
+          return res
+            .status(404)
+            .json({ commentnotexists: "Comment Does Not Exists" });
+        }
+        // GET Remove Index
+        const removeIndex = post.comments
+          .map(item => item._id.toString())
+          .indexOf(req.params.comment_id);
+
+        //splice out comment
+        post.comments.splice(removeIndex, 1);
+        post.save().then(post => res.json(post));
+      })
+      .catch(err => res.status(404).json({ postnotfound: "No Post Found" }));
+  }
+);
+
 module.exports = router;
